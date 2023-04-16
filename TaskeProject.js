bd5d885648dba9e85bd24ee97a019d284d24f069
Clayton <127148272+Clay2002@users.noreply.github.com>
2023-04-16 17:46:41 -0400

    Just some changes
    Added CSS and started making some functions in javascript. Messed around with the dark mode option that I am working on and will improve once I finish the javascript. Trying to focus on creating and making javascript functions and using the dom. I tried to do an array but I got lost and started by doing something not as complex so I could work my way up. Right now I am going to add a taskbar input. This will display more than one of the tasks I need to do and it will also append a button next to the input that will let you press and delete the input next to it. That will be the next big thing I need to focus on and work on. No more HTML or CSS. I just need to learn and focus on making stuff on javascript for this project and then do all the cool fun stuff later once Im done with JS.

--- TaskeProject.js
+++ TaskeProject.js
@@ -4,7 +4,36 @@
 function dark(){
 
 
-    let button = document.getElementById("darkmode-button").innerHTML = 'ON';
+    let title = document.getElementById("title");
+    let darkColor= document.getElementById("darkmode");
+    let dateColor = document.getElementById("date");
+    let button = document.getElementById("darkmode-button");
+
+  
+    if (button.innerHTML === 'ON'){
+       
+       
+        button.innerHTML = "OFF"
+
+        title.style["color"] = "black";
+        darkColor.style["color"] = "black";
+        dateColor.style["color"] = "black";
+
+        title.style["background-color"] = "#e6eaee";
+        darkColor.style ["background-color"] = "#e6eaee";
+        dateColor.style ["background-color"] = "#e6eaee";
+
+
+    } else {button.innerHTML = "ON"
+
+        title.style["color"] = "#B3B3B3";
+        darkColor.style["color"] = "#B3B3B3";
+        dateColor.style["color"] = "#B3B3B3";
+
+        title.style["background-color"] = "#404040";
+        darkColor.style ["background-color"] = "#404040";
+        dateColor.style ["background-color"] = "#404040";
+};
 
 
 };
@@ -13,20 +42,46 @@ function dark(){
 function add(){
 
     
-    var a = document.createElement('input');
+    let x = document.createElement("DIV");
+    x.setAttribute("type", "text");
     
+    document.body.appendChild(x);
+    
+   
+}
+
+//displays the date month / day / year
+
+var date = new Date();
+var current_date = (date.getMonth()+1) + "-" 
++ date.getDate() + "-" + date.getFullYear(); 
+document.getElementById("date").innerHTML = current_date;
+
+
+    
+
+////////////////////////
+
+
+
+
+
+function getInputValue() {
+
+
+    let inputValue = document.getElementById("textInput1").value;
+    document.getElementById("output").innerHTML = inputValue;
+
+
+
+
+    var button = document.createElement("button");
+    button.innerHTML = "-";
+
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(button);
 
 
 
 }
-
-
-
-
-    
-   
-    
-
-
-
 
