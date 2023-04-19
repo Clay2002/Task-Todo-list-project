ed7e2ebb847f19a93e700cc8e52dc563ee1d8e90
Clayton <127148272+Clay2002@users.noreply.github.com>
2023-04-19 14:56:58 -0400

    changes 3.0
    Just been working on javascript dom. I've been spending a lot of time on the dom and trying to figure out connecting online functions. Spend around 3 hours on something that just didn't end up working. I guess I learned some stuff from it which is good. I finally got a base working. Also added some CSS and hover effects with border-radius on the buttons so everything looks good. Going to change the display to have everything mainly in the center. It just seems like everything is on the left-right so I am going to work on that. Also, finishing the dark mode is another thing I want to work on, and going to add javascript to the checkbox and have it do something. Probably got like 5 more hours and going to stop adding stuff so I can move on and practice arrays if else and objects and loops in javascript so I can create a more complex and better working project.

--- TaskeProject.js
+++ TaskeProject.js
@@ -7,8 +7,15 @@ function dark(){
     let darkColor= document.getElementById("darkmode");
     let dateColor = document.getElementById("date");
     let button = document.getElementById("darkmode-button");
+    let list = document.getElementById("list");
+    let container2 = document.getElementById("container-2");
+    let container3 = document.getElementById("container-3");
+
+
 
     
+
+
   
     if (button.innerHTML === 'ON'){
        
@@ -18,21 +25,35 @@ function dark(){
         title.style["color"] = "black";
         darkColor.style["color"] = "black";
         dateColor.style["color"] = "black";
+        list.style["color"] = "black";
+        container2.style["color"] = "black";
+        container3.style["color"] = "black";
 
         title.style["background-color"] = "#e6eaee";
         darkColor.style ["background-color"] = "#e6eaee";
         dateColor.style ["background-color"] = "#e6eaee";
-      
+        list.style ["background-color"] = "#e6eaee";
+        container2.style["background-color"] = "#e6eaee";
+        container3.style["background-color"] = "#e6eaee";
+       
 
     } else {button.innerHTML = "ON"
 
         title.style["color"] = "#B3B3B3";
         darkColor.style["color"] = "#B3B3B3";
         dateColor.style["color"] = "#B3B3B3";
+        list.style ["color"] = "#B3B3B3";
+        container2.style["color"] = "#B3B3B3";
+        container3.style["color"] = "#B3B3B3";
 
         title.style["background-color"] = "#404040";
         darkColor.style ["background-color"] = "#404040";
         dateColor.style ["background-color"] = "#404040";
+        list.style ["background-color"] = "#404040";
+        container2.style  ["background-color"] = "#404040";
+        container3.style  ["background-color"] = "#404040";
+      
+
 };
 
 };
@@ -56,38 +77,143 @@ document.getElementById("date").innerHTML = current_date;
 function getInputValue() {
 
 
-   
-
-
 
     let inputValue = document.getElementById("textInput1").value;
     document.getElementById("output").innerHTML = inputValue;
+    
+};
+
+function getInputValue2(){
 
     let inputValue2 = document.getElementById("textInput2").value;
     document.getElementById("output2").innerHTML = inputValue2;
+}
+
+function getInputValue3(){
 
     let inputValue3 = document.getElementById("textInput3").value;
     document.getElementById("output3").innerHTML = inputValue3;
 
+
+}
+
+function getInputValue4(){
+    
     let inputValue4 = document.getElementById("textInput4").value;
     document.getElementById("output4").innerHTML = inputValue4;
+   
+}
 
 
+function makeDel(){
+
+    const element1 = document.getElementById("addButton1");
+    element1.innerHTML = "Change";
+
+
+    var button = document.createElement("button");
+    button.innerHTML = "delete";
+ 
+
+
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(button);
+    button.id = 'mainButton'
+   
+document.getElementById('mainButton').onclick = function(){
+
+
+    const mainButton = document.getElementById('mainButton');
+    const output = document.getElementById('output');
+
+    mainButton.remove();
+    output.remove();
+
+};
+
+};
+//button 2
+
+function del2(){
+
+    const element2 = document.getElementById("addButton2");
+    element2.innerHTML = "Change";
+
+    var button2 = document.createElement("button");
+    button2.innerHTML = "delete";
+
+    var body2 = document.getElementsByTagName("body")[0];
+    body2.appendChild(button2);
+    button2.id = 'mainButton2'
+
+    document.getElementById('mainButton2').onclick = function(){
+
+
+        const mainButton2= document.getElementById('mainButton2');
+        const output2 = document.getElementById('output2');
+    
+        mainButton2.remove();
+        output2.remove();
+    
+    };
+    
 
 };
 
 
+//button 3
+function del3(){
 
 
+    const element3 = document.getElementById("addButton3");
+    element3.innerHTML = "Change";
+
+    var button3 = document.createElement("button");
+    button3.innerHTML = "delete";
+
+    var body3 = document.getElementsByTagName("body")[0];
+    body3.appendChild(button3);
+    button3.id = 'mainButton3'
 
 
+    document.getElementById('mainButton3').onclick = function(){
+
+        const mainButton3= document.getElementById('mainButton3');
+        const output3 = document.getElementById('output3');
+    
+        mainButton3.remove();
+        output3.remove();
+    
+    };
+    
+};
 
 
+//button 4 
+function del4(){
 
 
+    const element4 = document.getElementById("addButton4");
+    element4.innerHTML = "Change";
 
+    var button4 = document.createElement("button");
+    button4.innerHTML = "delete";
 
+    var body4 = document.getElementsByTagName("body")[0];
+    body4.appendChild(button4);
+    button4.id = 'mainButton4'
 
+    document.getElementById('mainButton4').onclick = function(){
+
+        const mainButton4= document.getElementById('mainButton4');
+        const output4 = document.getElementById('output4');
+    
+        mainButton4.remove();
+        output4.remove();
+    
+    };
+    
+};
 
 
     
